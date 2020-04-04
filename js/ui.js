ee04a69801f7630db859d4dacd0456e6a1b7e4d6
jillBreau <jmbreau@mun.ca>
2020-04-03 23:06:55 -0230

    finish update-wish functions

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,62 @@
-var colourNames = ["Red", "Blue", "Green", "Pink", "Orange", "Purple"];
-
 var age = document.getElementById("age")
 var enterName = document.getElementById("enterName")
 var generateWish = document.getElementById("generateWish")
 
 function displayValue(element, value) {
-    document.getElementById(element).value = value;\
+    document.getElementById(element).value = value;
+}
+
+function generateMessage() {
+    console.log("!!!!!!!!!!!!!!!!")
+    var name = document.getElementById('nameView').value;
+    console.log(name)
+    var age = document.getElementById('ageView').value;
+    var ageSuffix = ordinalSuffix(age);
+    
+    var wish = document.getElementById('wishView')
+    wish.innerHTML = `Happy ${ageSuffix} Birthday ${name}!!`
+    wish.style.color = color
+}
+
+function ordinalSuffix(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
 }
 
 
-odds.onclick = function() { // functions the button
-
+Red.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'red'
+}
+Blue.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'blue'
+}
+Green.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'green'
+}
+Pink.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'magenta'
+}
+Orange.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'orangered'
+}
+Purple.onclick = function() {
+    var wish = document.getElementById('wishView')
+    wish.style.color = 'purple'
 }
 
 
