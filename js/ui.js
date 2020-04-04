a19b7af15aada518800e110b3164a5b97fec8fda
jillBreau <jmbreau@mun.ca>
2020-04-03 22:07:42 -0230

    add js

---
+++ js/ui.js
@@ -0,0 +1,17 @@
+var colourNames = ["Red", "Blue", "Green", "Pink", "Orange", "Purple"];
+
+var age = document.getElementById("age")
+var enterName = document.getElementById("enterName")
+var generateWish = document.getElementById("generateWish")
+
+function displayValue(element, value) {
+    document.getElementById(element).value = value;\
+}
+
+
+odds.onclick = function() { // functions the button
+
+}
+
+
+document.getElementById("ageView").value = age.value;
